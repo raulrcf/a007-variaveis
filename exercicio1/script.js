e320a8d35b843534e0212c5ec3cec066b8d9f2b4
Raul Cesar <raulcesar.rcf@gmail.com>
2022-07-19 21:15:30 -0300

    enviando novamente os arquivos do exercicio

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -2,7 +2,7 @@
 const nome = prompt("qual seu nome ? ")
 let idade = prompt("Qual a sua idade ?")
 
-//O tipo da variavel é string e é string por que nós digitamos a resposta no prompt.
+//O tipo da variavel é string e é string por que nós digitamos a resposta no prompt..
 
 prompt ("Olá, seu nome é   " + nome + " e sua idade é " + idade)
 
---
+++ exercicio2/script.js
@@ -0,0 +1,20 @@
+
+
+const perguntaA = "  Voce gosta de doce ?  "
+const perguntaB = "  Voce gosta de batata?  "
+const perguntaC = "  Voce gosta de Limao?  "
+
+const resposta1 = prompt (perguntaA)
+const resposta2 = prompt (perguntaB)
+const resposta3 = prompt (perguntaC)
+
+console.log(perguntaA,"-", resposta1)
+console.log(perguntaB,"-", resposta2)
+console.log(perguntaC,"-", resposta3)
+
+
+
+
+//alert ("Voce gosta de doce?:  "+ resposta1 + "   Voce gosta de batata?:   "+ resposta2 + "   Voce gosta de limao?:   " + resposta3)
+
+alert (perguntaA + resposta1 + perguntaB + resposta2 + perguntaC + resposta3)
